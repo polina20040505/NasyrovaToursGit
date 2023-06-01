4351082960887845fabf9f556b20ba4181bd5d4f
p-thtc <pathetiiiiic@gmail.com>
2023-06-01 11:10:40 +0700

    Добавьте файлы проекта.

---
+++ HotelsPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NasyrovaTours
+{
+    /// <summary>
+    /// Логика взаимодействия для HotelsPage.xaml
+    /// </summary>
+    public partial class HotelsPage : Page
+    {
+        public HotelsPage()
+        {
+            InitializeComponent();
+            DGridHotels.ItemsSource = NasyrovaSemenova_ToursBaseEntities1.GetContext().Hotel.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Hotel));
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var hotelsForRemoving = DGridHotels.SelectedItems.Cast<Hotel>().ToList();
+
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {hotelsForRemoving.Count()} элементов?", "Внимание",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+
+            {
+                try
+                {
+                    NasyrovaSemenova_ToursBaseEntities1.GetContext().Hotel.RemoveRange(hotelsForRemoving);
+                    NasyrovaSemenova_ToursBaseEntities1.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены!");
+
+                    DGridHotels.ItemsSource = NasyrovaSemenova_ToursBaseEntities1.GetContext().Hotel.ToList();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+                }
+            }
+
+            private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+            {
+                if (Visibility == Visibility.Visible)
+                {
+                NasyrovaSemenova_ToursBaseEntities1.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                    DGridHotels.ItemsSource = NasyrovaSemenova_ToursBaseEntities1.GetContext().Hotel.ToList();
+                }
+            }
+        }
+    }
